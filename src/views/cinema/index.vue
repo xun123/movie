c4ba54078055ee43c1dd395dd959a7c71318ffaa
xun123 <614897496@qq.com>
2019-09-13 12:25:04 +0800

    报错

---
+++ src/views/cinema/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="main">
+        <Header title="喵喵影院"/>
+        <div id="content">
+			<div class="cinema_menu">
+				<div class="city_switch">
+					全城 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="brand_swtich">
+					品牌 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+				<div class="feature_switch">
+					特色 <i class="iconfont icon-lower-triangle"></i>
+				</div>
+			</div>
+            <Cilist />
+        </div>
+
+        <Tabbar />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/c_header';
+import Tabbar from '@/components/c_tabbar';
+import Cilist from '@/components/c_cilist';
+
+export default {
+    name:'Cinema',
+    components:{
+        Header,
+        Tabbar,
+        Cilist
+    }
+}
+</script>
+
+<style lang="less" scoped>
+#content .cinema_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-around; align-items:center; background:white;}
+</style>
